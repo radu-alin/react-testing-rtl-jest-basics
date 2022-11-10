5b446bacccb879e7896f83a635c707edb3602139
Alin RADU <cristian.alin.radu@reea.net>
2022-11-10 10:27:50 +0200

    add info.md

---
+++ info.md
@@ -0,0 +1,5 @@
+Info
+
+tutorial: https://www.youtube.com/watch?v=T2sv8jXoP4s&list=PLC3y8-rFHvwirqe1KHFCHJ0RqNuN61SJd
+
+github: https://github.com/radu-alin/react-testing-rtl-jest-basics.git
--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -2,17 +2,18 @@ import { render, screen } from '@testing-library/react';
 
 import { Greet } from './Greet';
 
-// test, expect, are methods from Jest that CRA globaly provides in every test
-test('Greet renders correctly', () => {
-  render(<Greet />);
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />);
 
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test('Greet renders with a name', () => {
-  render(<Greet name="Alin" />);
+  test('renders with a name', () => {
+    render(<Greet name="Alin" />);
 
-  const textElement = screen.getByText(/hello alin/i);
-  expect(textElement).toBeInTheDocument();
+    const textElement = screen.getByText(/hello alin/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });

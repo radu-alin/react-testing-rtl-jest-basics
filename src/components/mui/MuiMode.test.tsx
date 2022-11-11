455077a735e326fa97e7c64ab47634e812a807a8
Alin RADU <cristian.alin.radu@reea.net>
2022-11-11 11:15:13 +0200

    add customRender, providers, MuiMode component and tests

---
+++ src/components/mui/MuiMode.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '../../test-utils';
+import { MuiMode } from './MuiMode';
+
+describe('MuiMode', () => {
+  test('renders text correctly', () => {
+    render(<MuiMode />);
+
+    const headingElement = screen.getByRole('heading', { level: 1 });
+
+    expect(headingElement).toHaveTextContent(/dark mode/i);
+  });
+});

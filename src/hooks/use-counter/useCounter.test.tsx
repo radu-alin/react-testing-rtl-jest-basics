ae452066fbff0d3f86d95a64c67e960fe1f42b4a
Alin RADU <cristian.alin.radu@reea.net>
2022-11-11 12:33:23 +0200

    add useCounter custom hook and tests

---
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -0,0 +1,38 @@
+import { renderHook, act } from '@testing-library/react';
+import { useCounter } from './useCounter';
+
+describe('useCounter', () => {
+  test('should render the initial count', () => {
+    const { result } = renderHook(useCounter);
+
+    const count = result.current.count;
+    expect(count).toBe(0);
+  });
+
+  test('should accept and render the same initial count', () => {
+    const { result } = renderHook(useCounter, {
+      initialProps: {
+        initialCount: 10,
+      },
+    });
+
+    const count = result.current.count;
+    expect(count).toBe(10);
+  });
+
+  test('should increment the count', () => {
+    const { result } = renderHook(useCounter);
+
+    act(() => result.current.increment());
+
+    expect(result.current.count).toBe(1);
+  });
+
+  test('should decrement the count', () => {
+    const { result } = renderHook(useCounter);
+
+    act(() => result.current.decrement());
+
+    expect(result.current.count).toBe(-1);
+  });
+});
